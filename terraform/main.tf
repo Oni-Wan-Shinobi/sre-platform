d74056ee41922610983058c510a7ff3dad1f2067
Oni-Wan-Shinobi <vladimirgriboedov855@gmail.com>
2026-05-25 21:53:51 +0000

    feat: k3s cluster - master on sre-node-1, worker on sre-node-2, open port 6443

--- terraform/main.tf
+++ terraform/main.tf
@@ -55,6 +55,12 @@ resource "hcloud_firewall" "main" {
     port      = "443"
     source_ips = ["0.0.0.0/0", "::/0"]
   }
+  rule {
+    direction  = "in"
+    protocol   = "tcp"
+    port       = "6443"
+    source_ips = ["0.0.0.0/0", "::/0"]
+  }
 }
 
 # Сервер (аналог EC2 в AWS)
