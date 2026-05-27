ab201601313cd70e5176550496a399da44ae1f73
Oni-Wan-Shinobi <vladimirgriboedov855@gmail.com>
2026-05-27 09:57:32 +0000

    feat: S3 backend for terraform state on Hetzner Object Storage

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,19 @@ terraform {
       version = "~> 1.45"
     }
   }
+backend "s3" {
+    bucket                      = "sre-terraform-state"
+    key                         = "terraform.tfstate"
+    region                      = "fsn1"
+    endpoints = {
+      s3 = "https://fsn1.your-objectstorage.com"
+    }
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    use_path_style            = true
+  }
 }
 
 provider "hcloud" {
@@ -100,3 +113,4 @@ resource "hcloud_server" "node2" {
 
   firewall_ids = [hcloud_firewall.main.id]
 }
+
